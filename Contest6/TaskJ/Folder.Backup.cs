6f1c6dc28dc5e64a9871816ef944cb211a05c623
Alexey Zobnin <ayuzobnin@edu.hse.ru>
2020-11-20 14:45:29 +0300

    Finished Contest 6.

--- Contest6/TaskJ/Folder.Backup.cs
+++ Contest6/TaskJ/Folder.Backup.cs
@@ -6,46 +6,75 @@ partial class Folder
 {
     internal class Backup
     {
+        internal Folder folder;
 
         public Backup(Folder folder)
         {
-            throw new NotImplementedException();
+            this.folder = new Folder();
+            this.folder.files.AddRange(folder.files);
+        }
+
+        public Folder Folder
+        {
+            get => folder;
+            internal set
+            {
+                folder = value;
+            }
         }
 
 
         public static void Restore(Folder folder, Backup backup)
         {
-            throw new NotImplementedException();
+            folder.files = backup.Folder.files;
         }
 
     }
 
     public void AddFile(string name, int size)
     {
-        throw new NotImplementedException();
+        files.Add(new File(name, size));
     }
     public void RemoveFile(File file)
     {
-        throw new NotImplementedException();
+        files.Remove(file);
     }
 
     public File this[int i]
     {
         get {
-            throw new NotImplementedException();
+            if (i < 0 || i >= files.Count)
+            {
+                throw new IndexOutOfRangeException("Not enough files or index less zero");
+            }
+            return files[i];
         }
     }
 
     public File this[string filename]
     {
         get {
-            throw new NotImplementedException();
+            foreach (var file in files)
+            {
+                if (filename.Equals(file.Name))
+                {
+                    return file;
+                }
+            }
+            throw new ArgumentException("File not found");
         }
     }
 
     public override string ToString()
     {
-        throw new NotImplementedException();
+        StringBuilder sb = new StringBuilder();
+        sb.Append("Files in folder:");
+        foreach (var file in files)
+        {
+            sb.Append(Environment.NewLine);
+            sb.Append(file.ToString());
+        }
+        return sb.ToString();
     }
 }
 
